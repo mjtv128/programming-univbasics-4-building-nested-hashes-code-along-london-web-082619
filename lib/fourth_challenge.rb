c699fbab5b41672b8f38af67c005760801c1cc71
jocular-source-7570 <mjtvan@gmail.com>
2019-08-09 11:29:26 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,8 +12,7 @@ def fourth_challenge
         {name: "Mercutio",
           age: "18",
           attitude: "hot-headed"}]
-      }
-   , 
+      }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
